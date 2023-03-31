b67b941c207b0788157aa3dd26b8f77faf04dc5e
sssjiang <zelinjiang@zelindeMacBook-Pro.local>
2023-03-31 11:52:09 +0800

    auto change the difficulty

--- block.py
+++ block.py
@@ -3,9 +3,10 @@ from time import time
 from utility.printable import Printable
 
 class Block(Printable):
-    def __init__(self, index, previous_hash, transactions, nonce, time=time()):
+    def __init__(self, index, previous_hash, transactions,difficulty,nonce, time=time()):
         self.index = index
         self.previous_hash = previous_hash
         self.timestamp = time
         self.transactions = transactions
         self.nonce = nonce
+        self.difficulty = difficulty
